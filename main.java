aa5039f876ea3b51706d7528c38c9c8c64971b01
Yogesh-017 <yogeshchavan1703@gmail.com>
2025-10-12 17:11:52 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,35 @@
+import java.util.*
+
+class student {
+ int age;
+String name;
+}
+
+class car {
+String name;
+int model no;
+}
+
+public class main{
+
+public static void main(String args[]){
+
+students st1=new student();
+st1.name="yogesh";
+st1.age=1'
+student st2=new student();
+st2.name="dhiraj";
+st2.age=2;
+
+car c1=new car();
+c1.name="BMW";
+c1.model=2021;
+
+System.out.println(st1.name);
+System.out.println(st1.age0);
+System.out.println(st2.name);
+System.out.println(st2.age);
+System.out.println(c1.name);
+System.out.println(c.model);
+}
+}
