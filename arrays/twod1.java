6fed1f23b8acd5bc990a96d1dd08b4aeaca82ccc
yogesh murlidhar chavan <yogeshchavan1703@gmail.com>
2025-10-16 23:45:54 +0530

    Add files via upload

---
+++ arrays/twod1.java
@@ -0,0 +1,16 @@
+public class twod1 {
+    public static void main(String args[]){
+          int[][] arr1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int sum=0;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                sum=arr1[i][j]+sum;
+            }
+        }
+        System.out.println(sum);
+    }
+}
