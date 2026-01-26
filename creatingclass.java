28fd9363bf637446d37375c66f2f296fb1ecf7f4
yogesh murlidhar chavan <yogeshchavan1703@gmail.com>
2026-01-26 23:57:43 +0530

    Add files via upload

---
+++ creatingclass.java
@@ -0,0 +1,18 @@
+package OOPS;
+
+class emp {
+    int EId;
+    float salary;
+}
+
+class department {
+    String HR;
+    String manager;
+}
+
+public class creatingclass {
+    public static void main(String args[]) {
+
+        // this is the example of declaring class only
+    }
+}
