a3b9781f299f8811550245d474e1074760a4fc6d
yogesh murlidhar chavan <yogeshchavan1703@gmail.com>
2026-01-26 23:58:24 +0530

    Add files via upload

---
+++ objectanddata.java
@@ -0,0 +1,20 @@
+package OOPS;
+
+class example {
+
+    int a;
+    String b;
+}
+
+public class objectanddata {
+
+    public static void main(String args[]) {
+
+        example e = new example();
+        e.a = 14;
+        e.b = "Yogesh";
+
+        System.out.println(e.a);
+        System.out.println(e.b);
+    }
+}
